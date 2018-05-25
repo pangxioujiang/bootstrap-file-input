b12c6de9386f89cf7e1e358d1983425f33e6ebc1
pangxioujiang <470552511@qq.com>
2018-05-25 20:06:47 +0800

    Add files via upload

---
+++ src/Facades/Packagetest.php
@@ -0,0 +1,10 @@
+<?php
+namespace Crab\Fileinput\Facades;
+use Illuminate\Support\Facades\Facade;
+class Packagetest extends Facade
+{
+    protected static function getFacadeAccessor()
+    {
+        return 'packagetest';
+    }
+}
--- src/FileinputServiceProvider.php
+++ src/FileinputServiceProvider.php
@@ -16,8 +16,14 @@ class FileinputServiceProvider extends ServiceProvider
 //        $this->publishes(); //发布到config 目录上
 //        $this->loadRoutesFrom(); //路由
 //        $this->loadMigrationsFrom();//数据库迁移
-        $this->loadViewsFrom(__DIR__.'/../views','Fileinput'); //视图
-        $this->publishes([__DIR__.'/../views' => base_path('resources/views/')]);  //发布视图到resources
+//        $this->loadViewsFrom(__DIR__.'/../views','Fileinput'); //视图
+//        $this->publishes([__DIR__.'/../views' => base_path('resources/views/')]);  //发布视图到resources
+
+        $this->loadViewsFrom(__DIR__ . '/views', 'Packagetest'); // 视图目录指定
+        $this->publishes([
+            __DIR__.'/views' => base_path('resources/views/vendor/packagetest'),  // 发布视图目录到resources 下
+            __DIR__.'/config/packagetest.php' => config_path('packagetest.php'), // 发布配置文件到 laravel 的config 下
+        ]);
     }
 
     /**
@@ -27,6 +33,9 @@ class FileinputServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        // 单例绑定服务
+        $this->app->singleton('packagetest', function ($app) {
+            return new Packagetest($app['session'], $app['config']);
+        });
     }
 }
